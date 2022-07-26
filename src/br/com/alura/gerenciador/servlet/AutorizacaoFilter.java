68ae288aa847a2260ff8f45a53532e6f09225419
Daniel dos Santos <daniel@automastersat.com.br>
2022-07-26 11:48:02 -0300

    servlet2 - adicionando um filter

---
+++ src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -0,0 +1,43 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AutorizacaoFilter
+ */
+@WebFilter("/entrada")
+public class AutorizacaoFilter extends HttpFilter implements Filter {
+       
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		
+		String paramAcao = request.getParameter("acao");
+		
+		HttpSession session = request.getSession();
+		boolean usuarioNaoLogado = (session.getAttribute("usuarioLogado") == null);
+		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+		if (usuarioNaoLogado && ehUmaAcaoProtegida) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+}
--- src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -20,13 +20,13 @@ public class UnicaEntradaServlet extends HttpServlet {
 			throws ServletException, IOException {
 		String paramAcao = request.getParameter("acao");
 		
-		HttpSession session = request.getSession();
-		boolean usuarioNaoLogado = (session.getAttribute("usuarioLogado") == null);
-		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
-		if (usuarioNaoLogado && ehUmaAcaoProtegida) {
-			response.sendRedirect("entrada?acao=LoginForm");
-			return;
-		}
+//		HttpSession session = request.getSession();
+//		boolean usuarioNaoLogado = (session.getAttribute("usuarioLogado") == null);
+//		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+//		if (usuarioNaoLogado && ehUmaAcaoProtegida) {
+//			response.sendRedirect("entrada?acao=LoginForm");
+//			return;
+//		}
 
 		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 
